af2d0b5b8bc4631755fc52565661d0ea1e046974
zy <3122971650@qq.com>
2020-08-23 11:15:44 +0800

    add conf and base api for aria2

---
+++ src/download/aria2/test_unit/aria2_test.py
@@ -0,0 +1,13 @@
+import json 
+import requests
+
+data = {
+    'jsonrpc':'2.0',
+    'id':'qwe',
+    'method':'aria2.addUri',
+    'params':[["http://download.kugou.com/download/kugou_pc"]]
+}
+data_s = json.dumps(data)
+r = requests.post('http://localhost:6800/jsonrpc',data_s)
+print(r.text)
+
